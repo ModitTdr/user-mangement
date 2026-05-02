85c28b573b8ae183a6c7af6ac2d36ac546999405
ModitTdr <modittuladhar@gmail.com>
2026-05-02 14:09:40 +0545

    feature: hide/show password icon for input type password

--- src/components/ui/Input/index.tsx
+++ src/components/ui/Input/index.tsx
@@ -1,5 +1,7 @@
 import type { FieldError, UseFormRegisterReturn } from "react-hook-form";
 import styles from "./style.module.scss";
+import { useState } from "react";
+import { EyeClosedIcon, EyeIcon } from "lucide-react";
 
 interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {
   label?: string;
@@ -7,17 +9,37 @@ interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {
   error?: FieldError;
   inputSize?: "sm" | "md" | "lg" | "full";
   className?: string;
+  type?: string;
 }
 
-const Input = ({ label, register, error, inputSize = "md", className, ...props }: InputProps) => {
+const Input = ({ label, register, error, inputSize = "md", className, type = "text", ...props }: InputProps) => {
+  const [showPassword, setShowPassword] = useState(false);
+  const isPassword = type === "password";
+
   return (
-    <div className={`${styles.input} ${styles[`input--${inputSize}`]} ${className || ""}`}>
+    <div className={`${styles.input} ${className || ""}`}>
       {label && <label htmlFor={props.name}>{label}</label>}
-      <input
-        id={props.name}
-        {...props}
-        {...register}
-      />
+
+      <div className={styles.input__wrapper}>
+        <input
+          className={`${styles.input__field} ${styles[`input__field--${inputSize}`]}`}
+          id={props.name}
+          type={isPassword ? (showPassword ? "text" : "password") : type}
+          {...props}
+          {...register}
+        />
+
+        {isPassword && (
+          <button
+            type="button"
+            className={styles.input__eye}
+            onClick={() => setShowPassword((prev) => !prev)}
+          >
+            {showPassword ? <EyeIcon size={16} /> : <EyeClosedIcon size={16} />}
+          </button>
+        )}
+      </div>
+
       {error && <span className={styles.input__error}>{error.message}</span>}
     </div>
   );
